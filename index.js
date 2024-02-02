7fd652316318761fb818399056ab9377bda499bc
marouan rtima <marouanrtima5@gmail.com>
2024-02-02 12:09:38 +0100

    Connect to a MongoDB Database Using Node.js

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = 3000;
+const mongoose = require("mongoose");
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/home.html", {root: __dirname});
+  res.sendFile("./views/home.html", { root: __dirname });
 });
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`);
-});
+mongoose
+  .connect(
+    "mongodb+srv://marouanrtima:DrDLsINWuA0nQtUk@cluster0.hwuplyx.mongodb.net/all-data?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}/`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
